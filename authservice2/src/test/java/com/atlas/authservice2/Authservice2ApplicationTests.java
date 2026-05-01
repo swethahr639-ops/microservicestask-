a9c4787e56b474469c3d188ffff02ada2c3d95f1
Pandu <pandu123@gmail.com>
2026-05-01 06:42:17 +0530

    jwt1 added application pom properties

---
+++ authservice2/src/test/java/com/atlas/authservice2/Authservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlas.authservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Authservice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
