89b00fd95980ecc283440e7f987285ae72132715
Pandu <pandu123@gmail.com>
2026-05-02 07:01:07 +0530

    jwt1 immplemented security config

--- authservice2/src/main/java/com/atlas/authservice2/config/SecurityConfig.java
+++ authservice2/src/main/java/com/atlas/authservice2/config/SecurityConfig.java
@@ -1,14 +1,41 @@
 package com.atlas.authservice2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.atlas.authservice2.dao.UserRepository;
+import com.atlas.authservice2.service.impl.MyUserDetailsServiceImpl;
+import com.atlas.authservice2.util.JwtRequestFilter;
+import com.twilio.http.Request;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	@Qualifier("handlerExceptionResolver")
+	private DelicatedAuthenticationEntryPoint authEntryPoint;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -16,4 +43,34 @@ public class SecurityConfig {
 
 	}
 
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new MyUserDetailsServiceImpl(userRepository, passwordEncoder());
+	}
+
+	public AuthenticationProvider authProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(passwordEncoder());
+		provider.setUserDetailsService(userDetailsService());
+
+		return provider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+		return configuration.getAuthenticationManager();
+
+	}
+
+	@Bean
+	public SecurityFilterChain springSecurityConfiguration(HttpSecurity http) throws Exception {
+
+		http.cors().and().addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+				.csrf(AbstractHttpConfigurer::disable)
+				.exceptionHandling(exception -> Exception.AuthentionEntryPoint(authEntryPoint))
+				.sessionManagement().session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.authorisedHttpRequest(Request -> reqquest.requestMatchers"").AuthenticationProvider(authProvider());
+		return http.build();
+	}
 }
