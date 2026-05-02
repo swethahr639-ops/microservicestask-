1550d06ec37f242733ad3e5410fa6a03367510c1
Pandu <pandu123@gmail.com>
2026-05-02 06:02:51 +0530

    added jtRequestFilter

--- authservice2/src/main/java/com/atlas/authservice2/util/JwtRequestFilter.java
+++ authservice2/src/main/java/com/atlas/authservice2/util/JwtRequestFilter.java
@@ -1,10 +1,17 @@
 package com.atlas.authservice2.util;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
+import com.atlas.authservice2.service.impl.MyUserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,11 +21,14 @@ import lombok.AllArgsConstructor;
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
+	private final MyUserDetailsServiceImpl myUserDetailsServiceImpl;
+
 	private final JwtTokenUtil jwtTokenUtil;
 
-	public JwtRequestFilter(JwtTokenUtil jwtTokenUtil) {
+	public JwtRequestFilter(JwtTokenUtil jwtTokenUtil, MyUserDetailsServiceImpl myUserDetailsServiceImpl) {
 		super();
 		this.jwtTokenUtil = jwtTokenUtil;
+		this.myUserDetailsServiceImpl = myUserDetailsServiceImpl;
 	}
 
 	@Override
@@ -30,6 +40,21 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 		try {
 			if (authorizationHeader.startsWith("Bearer ")) {
 				jwtToken = authorizationHeader.substring(7);
+				username = jwtTokenUtil.extractUsernameFromToken(jwtToken);
+
+				if (username != null && SecurityContextHolder.getContext().getAuthentication() == null
+						&& jwtTokenUtil.validateToken(jwtToken, username)) {
+
+					List<GrantedAuthority> authList = new ArrayList<>();
+					authList.add(new SimpleGrantedAuthority(jwtTokenUtil.getRoleFromToken(jwtToken)));
+					UsernamePasswordAuthenticationToken usernamePassordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+							username, null, authList);
+					usernamePassordAuthenticationToken
+							.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					SecurityContextHolder.getContext().setAuthentication(usernamePassordAuthenticationToken);
+				}
+				filterChain.doFilter(request, response);
+
 			}
 
 		} catch (Exception e) {
